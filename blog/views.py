14616e3b2d3cf3d8cd3e25de3f7a5b6d2d0becce
risav-pyakurel <pyakurel.risav@gmail.com>
2024-09-02 11:14:30 +0545

    adding pagination

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list,5)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(
         request,
         'blog/post/list.html',
@@ -12,11 +16,14 @@ def post_list(request):
     )
 
 
-def post_detail(request, id):
+def post_detail(request, year,month,day,post):
     post = get_object_or_404(
         Post,
-        id=id,
-        status=Post.Status.PUBLISHED
+        status=Post.Status.PUBLISHED,
+        slug =post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
     )
     return render(
         request,
